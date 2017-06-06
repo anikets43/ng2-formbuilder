e129c7aafa2fe61202aa3fed8c15a988549f397b
Aniket Kumar Agrawal <Aniket.K.Agrawal@aexp.com>
2017-06-07 01:04:36 +0530

    Added Form Render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,38 @@ import { Component, AfterViewInit } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   title = 'app works!';
 
+  options = {
+    defaultFields: [{
+      className: 'form-control',
+      label: 'First Name',
+      placeholder: 'Enter your first name',
+      name: 'first-name',
+      required: true,
+      type: 'text'
+    }, {
+      className: 'form-control',
+      label: 'Select',
+      name: 'select-1454862249997',
+      type: 'select',
+      multiple: 'true',
+      values: [{
+        label: 'Custom Option 1',
+        value: 'test-value'
+      }, {
+        label: 'Custom Option 2',
+        value: 'test-value-2'
+      }]
+    }, {
+      label: 'Radio',
+      name: 'select-1454862249997',
+      type: 'radio-group'
+    }]
+  };
+
+
   ngAfterViewInit() {
     const fbEditor = document.getElementById('fb-editor');
-    const formBuilder = $(fbEditor).formBuilder();
+    const formBuilder = $(fbEditor).formBuilder(this.options);
 
     document.getElementById('getJSON').addEventListener('click', function () {
       alert(formBuilder.actions.getData('json'));
@@ -18,8 +47,8 @@ export class AppComponent implements AfterViewInit {
   }
 
   getData() {
-    var fbEditor = document.getElementById('fb-editor');
-    var formBuilder = $(fbEditor).formBuilder();
+    const fbEditor = document.getElementById('fb-editor');
+    const formBuilder = $(fbEditor).formBuilder();
 
     alert(formBuilder.actions.getData('json'));
   }
